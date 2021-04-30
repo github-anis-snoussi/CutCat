ac401245eb128db14161c037899c564f7e215da8
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-04-30 22:44:52 +0100

    why I havent seen this before

--- app.py
+++ app.py
@@ -1,40 +1,60 @@
-from flask import Flask, escape, redirect, request, session, url_for
+from datetime import timedelta
 
-from redissession import RedisSessionInterface
+from redis import Redis
+from flask import Flask, render_template_string, request, session, redirect, url_for
+from flask_session import Session
 
+
+# Create the Flask application
 app = Flask(__name__)
-app.session_interface = RedisSessionInterface()
+
+# Details on the Secret Key: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
+# NOTE: The secret key is used to cryptographically-sign the cookies used for storing
+#       the session identifier.
+app.secret_key = 'BAD_SECRET_KEY'
+
+# Configure Redis for storing the session data on the server-side
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_REDIS'] = Redis(host='redis', port=6379)
+
+# Create and initialize the Flask-Session object AFTER `app` has been configured
+server_session = Session(app)
 
 
-@app.route('/')
-def index():
-    if 'username' in session:
-        return 'Logged in as %s' % escape(session['username'])
-    return 'You are not logged in'
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/set_email', methods=['GET', 'POST'])
+def set_email():
     if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
-    return '''
-        <form action="" method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
+        # Save the form data to the session object
+        session['email'] = request.form['email_address']
+        return redirect(url_for('get_email'))
+
+    return """
+        <form method="post">
+            <label for="email">Enter your email address:</label>
+            <input type="email" id="email" name="email_address" required />
+            <button type="submit">Submit</button
         </form>
-    '''
+        """
 
 
-@app.route('/logout')
-def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('index'))
+@app.route('/get_email')
+def get_email():
+    return render_template_string("""
+            {% if session['email'] %}
+                <h1>Welcome {{ session['email'] }}!</h1>
+            {% else %}
+                <h1>Welcome! Please enter your email <a href="{{ url_for('set_email') }}">here.</a></h1>
+            {% endif %}
+        """)
 
 
-# set the secret key.  keep this really secret:
-app.secret_key = b'\xe2\x92*\x1b\x96F\xf2\xafh^\xfd\xcf\xde\xb4f\xbd\x0b\xdf\xa1@#\xd4\xb1\x9c'
+@app.route('/delete_email')
+def delete_email():
+    # Clear the email stored in the session object
+    session.pop('email', default=None)
+    return '<h1>Session deleted!</h1>'
 
 
 if __name__ == '__main__':
