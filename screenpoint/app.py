342d38016dd5954be1346ca07fee4f4ce1cba4bc
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-05-03 16:17:59 -0400

    still looking for the best versions of packages and python

---
+++ screenpoint/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Flask inside Docker!!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=5000)
